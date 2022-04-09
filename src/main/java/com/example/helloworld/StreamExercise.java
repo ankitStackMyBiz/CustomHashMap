2b5b63b4083af8e949a4cf202a1f28aac6febcdb
ankitStackMyBiz <ankit.kumar@stackmybiz.com>
2022-04-09 23:10:43 +0530

    filter using stream

--- src/main/java/com/example/helloworld/StreamExercise.java
+++ src/main/java/com/example/helloworld/StreamExercise.java
@@ -1,5 +1,8 @@
 package com.example.helloworld;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamExercise {
 
 
@@ -9,26 +12,41 @@ public class StreamExercise {
         carHashMap.put("Audi", 2);
         carHashMap.put("BMW", 5);
         carHashMap.put("Maruti", 4);
-        carHashMap.put("Suzuki", 5);
+        carHashMap.put("Suzuki", 7);
         carHashMap.put("Aston", 1);
         carHashMap.put("apple", 0);
 
+        filterMap(carHashMap);
 
-       // System.out.println(carHashMap);
 
-        //System.out.println(carHashMap.entrySet());
-        System.out.println(carHashMap.keySet());
-        System.out.println(carHashMap.values());
+        AnkitHashMap<String, Integer> fruits = new AnkitHashMap<>();
+
+        fruits.put("Apple", 200);
+        fruits.put("apple", 80);
+        fruits.put("Mango", 40);
+        fruits.put("angoor", 70);
+        fruits.put("Melon", 10);
+        fruits.put("Grapes", 100);
+
+        filterMap(fruits);
+
+    }
+
+    private static void filterMap(AnkitHashMap<String, Integer> hashMap) {
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("\n\nkeySet : "+hashMap.keySet());
+        System.out.println("valueSet : "+hashMap.values());
+
+
         //Using stream to iterate and remove the entries starting with "a"
+        System.out.println("\nAfter applying filter the hashmap - key starts with 'a' will be filtered out :: ");
 
-        System.out.println("After delete(key starts with a : " );
-        carHashMap.keySet().stream().forEach(k -> {
-            if(k.startsWith("a")){
-                carHashMap.remove(k);
-            }
-        });
+        //list
+        List<String> filteredList = hashMap.keySet().stream()
+                .filter(e -> e.startsWith("a"))
+                .collect(Collectors.toList());
 
-        System.out.println(carHashMap.keySet());
+        System.out.println("Filtered out with key starts with 'a'  : "+filteredList);
     }
 
 
